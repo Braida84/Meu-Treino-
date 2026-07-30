46104ab8cf7089eb7e46eeaca8b3b3d8b92e9c0a
Braida84 <brunoluizbraida@gmail.com>
2026-07-30 00:17:58 -0300

    v5.3.1: atualizar cache para troca individual de exercícios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-treino-pro-v5.3';const ASSETS=['./','./index.html','./app.js','./v51.js','./equipment-import.js','./v53.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='meu-treino-pro-v5.3.1';const ASSETS=['./','./index.html','./app.js','./v51.js','./equipment-import.js','./v53.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
